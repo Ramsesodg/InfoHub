5c7a1765d01ca5132c26d853d7a662b1c3ab81fd
yasmine ouedraogo <77838738+yasminealt@users.noreply.github.com>
2024-10-10 07:12:17 +0000

    Delete Documents/FormationDjango directory
    je n'ai plus besoin

--- Documents/FormationDjango/week 9/Days 4_5/Mini_projet_hotel/account/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=70, label="Nom d'Utilisateur")
-    password = forms.CharField(max_length=70, widget=forms.PasswordInput, label="Mot de passe")
--- Documents/FormationDjango/week 9/Days 4_5/Mini_projet_hotel/account/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.login_page, name='login_page'),
-    path('logout/', views.logout_user, name='logout')
-]
