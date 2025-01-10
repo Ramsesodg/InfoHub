a995228654494fb9d98a0e6a4f9762cd6f6a1380
Ramsesodg <Ouedraogoadanfranck@gmail.com>
2025-01-10 22:35:55 +0000

    modal

--- resources/views/pages/fiches.blade.php
+++ resources/views/pages/fiches.blade.php
@@ -120,4 +120,7 @@
         </div>
     </div>
 </div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
--- resources/views/pages/profils.blade.php
+++ resources/views/pages/profils.blade.php
@@ -3,7 +3,6 @@
     'elementActive' => 'profils'
 ])
 
-
 @section('content')
 <div class="container">
     <h1>Gestion des Profils</h1>
@@ -14,43 +13,51 @@
     </form>
 
     <!-- Bouton de création -->
-    <button class="btn btn-primary my-3" data-bs-toggle="collapse" data-bs-target="#createProfileForm">
+    <button class="btn btn-primary my-3" data-bs-toggle="modal" data-bs-target="#createProfileModal">
         Ajouter un Nouveau Profil
     </button>
 
-    <!-- Formulaire de création -->
-    <div class="collapse" id="createProfileForm">
-        <div class="card card-body">
-            <form action="{{ route('profils.store') }}" method="POST">
-                @csrf
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="nom">Nom</label>
-                        <input type="text" name="nom" class="form-control" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="prenom">Prénom</label>
-                        <input type="text" name="prenom" class="form-control" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" class="form-control" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="telephone">Téléphone</label>
-                        <input type="text" name="telephone" class="form-control" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="nom_utilisateur">Nom Utilisateur</label>
-                        <input type="text" name="nom_utilisateur" class="form-control" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="pass">Mot de Passe</label>
-                        <input type="password" name="pass" class="form-control" required>
-                    </div>
+    <!-- Modal pour Ajouter un Nouveau Profil -->
+    <div class="modal fade" id="createProfileModal" tabindex="-1" aria-labelledby="createProfileModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createProfileModalLabel">Ajouter un Nouveau Profil</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <button type="submit" class="btn btn-success">Créer</button>
-            </form>
+                <div class="modal-body">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label for="nom">Nom</label>
+                                <input type="text" name="nom" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="prenom">Prénom</label>
+                                <input type="text" name="prenom" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="telephone">Téléphone</label>
+                                <input type="text" name="telephone" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="nom_utilisateur">Nom Utilisateur</label>
+                                <input type="text" name="nom_utilisateur" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="pass">Mot de Passe</label>
+                                <input type="password" name="pass" class="form-control" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success">Créer</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -67,7 +74,6 @@
             </tr>
         </thead>
         <tbody>
-
             <tr>
                 <td></td>
                 <td></td>
@@ -76,7 +82,7 @@
                 <td></td>
                 <td>
                     <!-- Bouton Modifier -->
-                    <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editModal ">
+                    <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editModal">
                         Modifier
                     </button>
 
@@ -131,4 +137,7 @@
         </tbody>
     </table>
 </div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
