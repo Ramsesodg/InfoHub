ef8ec9d33724a1acf9a68487dc806f48ae7dc809
yasminealt <oyasmine283@gmail.com>
2025-01-16 17:59:41 +0000

    Ajout du fichier .gitignore et nettoyage

--- Downloads/InfoHub-main/resources/views/layouts/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright © <script>
-                        document.write(new Date().getFullYear())
-                    </script> <a href=""></a>CodingRoom_By_RamsesODG</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 0.0.1
-    </div>
-  </footer>
